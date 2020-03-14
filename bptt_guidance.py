bug eta instead of position in new calculation of ye

bptt_guidance.py
@@ -311,7 +311,7 @@ class BPTT_Controller():
         psi = eta[:, 2]
         psi = tf.where(tf.greater(tf.abs(psi), np.pi), (tf.math.sign(psi))*(tf.math.abs(psi)-2*np.pi), psi)
 
-        ye = -(position[:, 0] - self.train_target[:, 1])*tf.math.sin(self.random_ak) + (position[:, 1] - self.train_target[:, 2])*tf.math.cos(self.random_ak)
+        ye = -(eta[:, 0] - self.train_target[:, 1])*tf.math.sin(self.random_ak) + (eta[:, 1] - self.train_target[:, 2])*tf.math.cos(self.random_ak)
 
         psi = tf.reshape(psi, [self.batch_size, 1])
         upsilon = tf.reshape(upsilon, [self.batch_size, 3])